b71be7c3fc0ea2ba4c7f6088600cf5264a0d901e
Ali <aliaboli6749@gmail.com>
2023-09-08 12:45:55 +0330

    Add project files.

---
+++ Design/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Design.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
